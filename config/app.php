b357243f21e1abd4329afcfc242c64ad39a441aa
paulocescar <paulo.c93@gmail.com>
2022-07-11 14:07:47 -0300

    teste

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
         Laratrust\LaratrustServiceProvider::class,
-        JasonGuru\LaravelMakeRepository\Repository\BaseRepository::class
         
         /*
          * Package Service Providers...
